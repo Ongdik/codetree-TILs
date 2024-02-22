a27eedb8d1025d85ded1330fd94d9396ccb743f5
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-02-22 20:30:21 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240222/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
+++ 240222/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -3,24 +3,24 @@
 #include <algorithm>
 using namespace std;
 
-int n,m;
-int graph[101][101]{0};
-bool visited[101][101]{false};
-int dx[2]{1, 0};
-int dy[2]{0, 1};
+int n, m;
+int graph[101][101]{ 0 };
+bool visited[101][101]{ false };
+int dx[2]{ 1, 0 };
+int dy[2]{ 0, 1 };
 bool check = false;
 
 void dfs(int x, int y) {
-    if (visited[y][x]) return;
+    if (visited[y][x] || graph[y][x] == 0) return;
 
     visited[y][x] = true;
-    if (y == n-1 && x == n-1) check = true;
+    if (y == n - 1 && x == n - 1) check = true;
 
-    for (int i=0; i<2; i++) {
-        int nx = x+dx[i];
-        int ny = y+dy[i];
-        if (nx>=0 && ny>=0 && nx<n && ny<n) {
-            if (!visited[ny][nx] && graph[ny][nx] != 0) dfs(ny,nx);
+    for (int i = 0; i < 2; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        if (nx >= 0 && ny >= 0 && nx < n && ny < n) {
+            if (!visited[ny][nx] && graph[ny][nx] != 0) dfs(nx, ny);
         }
     }
 }
@@ -31,13 +31,13 @@ int main() {
 
     cin >> n >> m;
 
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<n; j++) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             cin >> graph[i][j];
         }
     }
-    dfs(0,0);
-    
+    dfs(0, 0);
+
     if (check) cout << 1;
     else cout << 0;
 
