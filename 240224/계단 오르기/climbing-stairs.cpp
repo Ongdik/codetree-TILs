b4d127ff31ce2a0f75ed8e2671cd73a584bcf747
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-02-24 13:15:56 +0900

    Title: 계단 오르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240224/계단 오르기/climbing-stairs.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n;
+int dp[1001]{0,0,1,1,1,2};
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+    for (int i=5; i<=n; i++) {
+        dp[i] = dp[i-3]+1 +dp[i-2]+1;
+    }
+
+    cout << dp[n];
+    return 0;
+}
