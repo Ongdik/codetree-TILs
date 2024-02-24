b25fdc964f2599f675b0ee34b744d2d549c7bcbd
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-02-24 13:17:09 +0900

    Title: 계단 오르기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240224/계단 오르기/climbing-stairs.cpp
+++ 240224/계단 오르기/climbing-stairs.cpp
@@ -3,8 +3,9 @@
 #include <algorithm>
 using namespace std;
 
+#define ll long long
+
 int n;
-int dp[1001]{0,0,1,1,1,2};
 
 int main() {
     ios_base::sync_with_stdio(0);
@@ -12,7 +13,7 @@ int main() {
 
     cin >> n;
     for (int i=5; i<=n; i++) {
-        dp[i] = dp[i-3]+dp[i-2];
+        dp[i] = (dp[i-3]+dp[i-2])%10007;
     }
 
     cout << dp[n];
