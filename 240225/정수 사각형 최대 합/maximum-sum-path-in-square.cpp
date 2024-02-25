ffbde7ecd2cc68918ca331d50038d8f23925304e
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-02-25 21:15:03 +0900

    Title: 정수 사각형 최대 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240225/정수 사각형 최대 합/maximum-sum-path-in-square.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+int arr[101][101]{0};
+int dp[101][101]{0};
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+
+    for (int i=1; i<=n; i++) {
+        for (int j=1; j<=n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    dp[1][1] = arr[1][1];
+
+    for (int i=2; i<=n; i++) {
+        dp[1][i] = dp[1][i-1] + arr[1][i];
+        dp[i][1] = dp[i-1][1] + arr[i][1];
+    }
+    
+    int ans = 0;
+    for (int i=2; i<=n; i++) {
+        for (int j=2; j<=n; j++) {
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])+arr[i][j];
+            ans = max(ans, dp[i][j]);
+        }
+    }
+
+    cout << ans;
+    
+
+    return 0;
+}
