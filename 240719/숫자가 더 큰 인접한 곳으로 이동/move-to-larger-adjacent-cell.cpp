0a6650a8c14744c3fe6c2692e84253f9d8d89127
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-07-19 01:46:35 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240719/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int arr[101][101]{0};
+int dx[4]{0,0,-1,1};
+int dy[4]{-1,1,0,0};
+int n;
+vector<int> ans;
+
+void move(int x, int y, int curNum) {
+
+    for (int i=0; i<4; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+
+        if (nx >= 1 && ny >= 1 && nx <= n && ny <=n ) {
+            if (curNum < arr[ny][nx]) {
+                ans.push_back(arr[ny][nx]);
+                move(nx, ny, arr[ny][nx]);
+                break;
+            }
+        }
+    }
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int r, c;
+    cin >> n >> r >> c;
+
+    for (int i=1; i<=n; i++) {
+        for (int j=1; j<=n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    ans.push_back(arr[c][r]);
+
+    move(r, c, arr[c][r]);
+
+    for (int i : ans) {
+        cout << i << " ";
+    }
+
+    return 0;
+}
